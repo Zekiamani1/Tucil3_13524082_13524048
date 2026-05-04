6aeb723544a4447e4a2c5cae25acfddb6589fef2
zekiamani1 <zekiamanioke@gmail.com>
2026-05-04 12:15:27 +0700

    fix: fix move, and some stuff too

--- src/grid.go
+++ src/grid.go
@@ -7,6 +7,8 @@ import (
 	"unicode"
 )
 
+var peta *Grid
+
 type Tipe int
 
 const (
@@ -43,21 +45,48 @@ const (
 	bawah
 )
 
+var Allarah = []Arah{
+	kiri,
+	kanan,
+	atas,
+	bawah,
+}
+
 func (p *Player) move(arah Arah) error { //kalo false berarti gabisa lewat situ
 	if p == nil || p.position == nil {
 		return errors.New("player or position is nil")
 	}
 	temp := p.position
-	for temp != nil && temp.tipe != TipeBlock {
+	for temp != nil {
+		stop := false
 		switch arah {
 		case kiri:
+			if temp.Kiri.tipe == TipeBlock {
+				stop = true
+				break
+			}
 			temp = temp.Kiri
 		case kanan:
+			if temp.Kanan.tipe == TipeBlock {
+				stop = true
+				break
+			}
 			temp = temp.Kanan
 		case atas:
+			if temp.Atas.tipe == TipeBlock {
+				stop = true
+				break
+			}
 			temp = temp.Atas
 		case bawah:
+			if temp.Bawah.tipe == TipeBlock {
+				stop = true
+				break
+			}
 			temp = temp.Bawah
+		}
+		if stop {
+			break
 		}
 		if temp == nil {
 			return errors.New("cannot move: reached boundary")
@@ -72,10 +101,9 @@ func (p *Player) move(arah Arah) error { //kalo false berarti gabisa lewat situ
 	}
 	return nil
 }
-func createGrid() *Grid {
+func createGrid() (firstgrid *Grid, start *Grid, end *Grid) {
 	var X int
 	var Y int
-	var start *Grid
 	fmt.Scan(&X, &Y)
 	grid := make([][]*Grid, X)
 	for i := 0; i < X; i++ {
@@ -83,7 +111,7 @@ func createGrid() *Grid {
 		fmt.Scanln(&temp)
 		input := []rune(temp)
 		if len(input) != Y {
-			return nil
+			return nil, nil, nil
 		}
 		grid[i] = make([]*Grid, Y)
 		for j := 0; j < Y; j++ {
@@ -99,6 +127,7 @@ func createGrid() *Grid {
 				temp2 = &Grid{Constraint: 0, tipe: TipeLava, coordinateX: j, coordinateY: i}
 			case input[j] == 'O':
 				temp2 = &Grid{Constraint: 0, tipe: TipeGoal, coordinateX: j, coordinateY: i}
+				end = temp2
 			case input[j] == 'Z':
 				temp2 = &Grid{Constraint: 0, tipe: TipeStart, coordinateX: j, coordinateY: i}
 				start = temp2
@@ -123,7 +152,8 @@ func createGrid() *Grid {
 			}
 		}
 	}
-	return start
+	firstgrid = grid[0][0]
+	return
 }
 func (g *Grid) calculateEuclideanDistance(other *Grid) float64 {
 	return math.Sqrt(math.Pow(float64(other.coordinateX)-float64(g.coordinateX), 2) + math.Pow(float64(other.coordinateY)-float64(g.coordinateY), 2))
@@ -137,10 +167,12 @@ func (g *Grid) printGrid() {
 			switch {
 			case itu.tipe == TipeBlock:
 				fmt.Print("X")
+			case itu.tipe == TipeGoal:
+				fmt.Print("O")
 			case itu.tipe == TipeLava:
 				fmt.Print("L")
 			case itu.tipe == TipeStart:
-				fmt.Print("S")
+				fmt.Print("Z")
 			case itu.tipe == TipeEmpty:
 				fmt.Print(itu.Constraint)
 			}
@@ -151,7 +183,11 @@ func (g *Grid) printGrid() {
 	}
 }
 func main() {
-	fmt.Println()
-	start := createGrid()
+	firstgrid, start, end := createGrid()
 	player := Player{position: start}
+	peta = firstgrid
+	player.bgfs(end)
+	// println()
+	// firstgrid.printGrid()
+
 }
